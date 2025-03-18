6256b4c3120385e75c9411c10d12fcbe34d01d23
taejin7088 <gog70888@gmail.com>
2025-03-18 19:16:20 +0900

    refator : url 환경변수 설정

--- src/app/api/common/getRotationChampions.ts
+++ src/app/api/common/getRotationChampions.ts
@@ -1,11 +1,12 @@
-import { INTERNAL_API_BASE } from '@/constants/url';
 import { ChampionCardType } from '@/types/championsType';
 import { CustomErr } from '@/types/customObj';
 
 //로테이션챔피언을 보내주는 함수
 //에러발생시(data.state === 500)
 export const getRotationChampions = async (): Promise<ChampionCardType[]> => {
-  const response = await fetch(`${INTERNAL_API_BASE}rotation`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/rotation`
+  );
   const data: Record<string, ChampionCardType> | CustomErr =
     await response.json();
 
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,2 @@
-export const INTERNAL_API_BASE = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}/api/`
-  : 'http://localhost:3000/api/';
 export const IMG_URL_BASE =
   'https://ddragon.leagueoflegends.com/cdn/15.5.1/img/';
