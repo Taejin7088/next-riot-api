406b2d841cef6d909b06295e4cb1f44e068cfcf4
taejin7088 <gog70888@gmail.com>
2025-03-18 15:11:31 +0900

    feat : 환경변수 vercel링크추가

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -1,3 +1,5 @@
-export const INTERNAL_API_BASE = 'http://localhost:3000/api/';
+export const INTERNAL_API_BASE = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}/api/`
+  : 'http://localhost:3000/api/';
 export const IMG_URL_BASE =
   'https://ddragon.leagueoflegends.com/cdn/15.5.1/img/';
