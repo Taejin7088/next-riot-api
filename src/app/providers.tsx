35ba99a8b74fcc4029f7e755f437f35748521757
taejin7088 <gog70888@gmail.com>
2025-03-18 21:16:10 +0900

    fix : 클라이언트에서 쿼리클라이언트 계속 새로생성되는버그수정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,10 @@
-// In Next.js, this file would be called: app/providers.jsx
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function makeQueryClient() {
@@ -14,8 +17,21 @@ function makeQueryClient() {
   });
 }
 
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // 서버에서 요청이 들어오면 항상 새로운 쿼리클라이언트
+    return makeQueryClient();
+  } else {
+    //클라이언트에서 요청이 들어왔을때 원래 있던게 있으면 있던거 보내주고 없으면 새로 만들어주기
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = makeQueryClient();
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
