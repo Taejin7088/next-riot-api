3dd7cfba54fc5a1b2a7573ea08f945ac10f42b07
taejin7088 <gog70888@gmail.com>
2025-03-18 19:29:09 +0900

    fix : rotation요청 url 환경변수 제거

--- src/app/api/common/getRotationChampions.ts
+++ src/app/api/common/getRotationChampions.ts
@@ -4,9 +4,7 @@ import { CustomErr } from '@/types/customObj';
 //로테이션챔피언을 보내주는 함수
 //에러발생시(data.state === 500)
 export const getRotationChampions = async (): Promise<ChampionCardType[]> => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/rotation`
-  );
+  const response = await fetch('/api/rotation');
   const data: Record<string, ChampionCardType> | CustomErr =
     await response.json();
 
