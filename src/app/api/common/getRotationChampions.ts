42eed59bf74fa934ab69669ee781e83d52b0904d
taejin7088 <gog70888@gmail.com>
2025-03-19 10:46:48 +0900

    refator : 로테이션함수에 nostore추가

--- src/app/api/common/getRotationChampions.ts
+++ src/app/api/common/getRotationChampions.ts
@@ -4,7 +4,7 @@ import { CustomErr } from '@/types/customObj';
 //로테이션챔피언에 쓰이는 api함수(CSR)
 //에러발생시(data.state === 500)
 export const getRotationChampions = async (): Promise<ChampionCardType[]> => {
-  const response = await fetch('/api/rotation');
+  const response = await fetch('/api/rotation', { cache: 'no-store' });
   const data: Record<string, ChampionCardType> | CustomErr =
     await response.json();
 
